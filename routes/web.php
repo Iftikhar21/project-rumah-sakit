c774f8863c99bd3fd5af4bae37c13e7dfccc61dc
TUF GAMING F15 <iftikharazharchaudhry@gmail.com>
2025-08-13 11:27:11 +0700

    create

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\DokterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dktr.index');
 });
 
 Route::resource('dktr',DokterController::class);
